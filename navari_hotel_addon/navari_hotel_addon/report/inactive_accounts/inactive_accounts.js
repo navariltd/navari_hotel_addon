8ad3fc505e4fb787f7972e5c6daf64437d4b6ff2
Brian Mwambia <brianmwambia3@gmail.com>
2025-08-14 13:28:24 +0300

    fix:added Last Activity option

--- navari_hotel_addon/navari_hotel_addon/report/inactive_accounts/inactive_accounts.js
+++ navari_hotel_addon/navari_hotel_addon/report/inactive_accounts/inactive_accounts.js
@@ -2,30 +2,33 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Inactive Accounts"] = {
-	"filters": [
-		{
-			"fieldname": "doctype",
-			"label": __("Doctype"),
-			"fieldtype": "Select",
-			"default": "Quotation",
-			"options": "Quotation\nSales Order\nSales Invoice",
-		},
-		{
-			"fieldname": "last_order_period",
-			"label": "Last Order Period",
-			"fieldtype": "Select",
-			"options": "\nAll\nLast Week\nLast Month\nLast Year",
-			"default": "All"
-		},
-		{
-			"fieldname": "last_order_from",
-			"label": "From Date",
-			"fieldtype": "Date"
-		},
-		{
-			"fieldname": "last_order_to",
-			"label": "To Date",
-			"fieldtype": "Date"
-		}
-	]
+  filters: [
+    {
+      fieldname: "doctype",
+      label: __("Doctype"),
+      fieldtype: "Select",
+      default: "Last Activity (All)",
+      options: "Last Activity (All)\nQuotation\nSales Order\nSales Invoice",
+      description: __(
+        "Select a single doctype or choose 'Last Activity (All)' to check across all"
+      ),
+    },
+    {
+      fieldname: "last_order_period",
+      label: "Last Order Period",
+      fieldtype: "Select",
+      options: "\nAll\nLast Week\nLast Month\nLast Year",
+      default: "All",
+    },
+    {
+      fieldname: "last_order_from",
+      label: "From Date",
+      fieldtype: "Date",
+    },
+    {
+      fieldname: "last_order_to",
+      label: "To Date",
+      fieldtype: "Date",
+    },
+  ],
 };
